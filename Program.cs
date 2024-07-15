f176c0ece1a05d090979e94c4da1012e590938e8
Furka <50808593+FirFurka@users.noreply.github.com>
2024-07-15 20:03:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ChangeWallpaper 
+{
+    internal class ChangeWallpaper
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, string vParam, UInt32 winIni);
+        private static void Main(string[] args)
+        {
+            string Path = ""; 
+            SystemParametersInfo(0x14, 0, Path, 0x01 | 0x02);
+        }
+    }
+}
